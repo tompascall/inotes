25b5c4d5ff332e0801d8e51bd8c783158522101d
tompascall <tompascall@gmail.com>
2015-04-05 19:08:00 +0200

    testing firebase

--- www/spike/js/app.js
+++ www/spike/js/app.js
@@ -4,46 +4,38 @@ angular.module('fb-test', ['ngHolder'])
 
 .controller('FbController', function ($scope, $timeout) {
   var database = new Firebase('https://scorching-torch-6342.firebaseio.com/');
-  var usersRef = database.child("test/users");
+  var providersRef = database.child("test/providers/tutors/");
+  $scope.providers = [];
 
-  usersRef.set({
-    '0': {
-      full_name: "Samson Godfri Gnanasegaran",
-      rating: 4
-    },
-    '1': {
-      full_name: "Adam Kelly",
-      rating: 3,
-    }
+  providersRef.set(null); // clear database
+
+  providersRef.push({
+    full_name: "Samson Godfri Gnanasegaran",
+    rating: 4,
+    objects: ['']
   });
 
-  usersRef.on("value", function(snapshot) {
+  providersRef.push({
+    full_name: "Adam Kelly",
+    rating: 3,
+    objects: ['']
+  })
+
+  providersRef.on("child_added", function(snapshot) {
     $timeout(function() {
-      $scope.users = snapshot.val();
+      $scope.providers.push(snapshot.val());
     }, 0);
   }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
   });
 
 
-  $scope.addUser = function () {
-    // Keys must be non-empty strings and can't contain ".", "#", "$", "/", "[", or "]"
-    // The $scope.users object contains $$hashKey angular key, so we have to filter it out
-    var users = $scope.users.map(function (user) {
-      return {
-        full_name: user.full_name,
-        rating: user.rating
-      };
+  $scope.addProvider = function () {
+    providersRef.push({
+      full_name: $scope.newProviderName,
+      rating: $scope.newProviderRating,
+      objects: ['']
     });
-
-    users.push({
-      full_name: $scope.newUserName,
-      date_of_birth: $scope.rating
-    });
-
-    usersRef.set(users);
-
-    $scope.rating = $scope.newUserName = '';
-
+    $scope.newProviderRating = $scope.newProviderName = $scope.objects = '';
   };
 });
